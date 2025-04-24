930a9f0e7b79a16041cb322cc646718bfca79fa8
root <luismichelpcontacto@gmail.com>
2025-04-24 02:59:41 +0000

    Case 4 Fix

--- shell_backup.c
+++ shell_backup.c
@@ -1,72 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
-#define MAX_COMMAND_LENGTH 1024
+#define MAX_LINE 1024
+#define MAX_ARGS 64
 
 /**
- * main - Simple Shell 0.1
- * Return: Always 0
+ * trim_whitespace - Elimina espacios al inicio y final de una cadena
+ * @str: Cadena de entrada
+ * Return: Puntero a la cadena recortada
+ */
+char *trim_whitespace(char *str)
+{
+    char *end;
+
+    while (*str == ' ' || *str == '\t' || *str == '\n')
+        str++;
+
+    if (*str == '\0')
+        return str;
+
+    end = str + strlen(str) - 1;
+    while (end > str && (*end == ' ' || *end == '\t' || *end == '\n'))
+        end--;
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+/**
+ * parse_line - Divide una línea en argumentos
+ * @line: Línea de entrada
+ * @args: Array donde se guardan los argumentos
+ */
+void parse_line(char *line, char **args)
+{
+    int i = 0;
+    line = trim_whitespace(line);
+    args[i] = strtok(line, " \t");
+    while (args[i] != NULL && i < MAX_ARGS - 1)
+    {
+        i++;
+        args[i] = strtok(NULL, " \t");
+    }
+    args[i] = NULL;
+}
+
+/**
+ * main - Bucle principal de la shell
  */
 int main(void)
 {
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	pid_t pid;
-	char *args[] = {NULL, NULL};
-	extern char **environ;
-	int interactive = isatty(STDIN_FILENO);
+    char line[MAX_LINE];
+    char *args[MAX_ARGS];
+    pid_t pid;
+    int status;
 
-	while (1)
-	{
-		if (interactive)
-		{
-			printf("#cisfun$ ");
-			fflush(stdout);
-		}
+    while (1)
+    {
+        printf("#cisfun$ ");
+        fflush(stdout);
 
-		read = getline(&line, &len, stdin);
+        if (fgets(line, MAX_LINE, stdin) == NULL)
+        {
+            if (feof(stdin))
+                break;
+            continue;
+        }
 
-		if (read == -1)
-		{
-			if (interactive)
-				printf("\n");
-			break;
-		}
+        char *clean_line = trim_whitespace(line);
+        if (strlen(clean_line) == 0)
+            continue;
 
-		if (line[read - 1] == '\n')
-			line[read - 1] = '\0';
+        parse_line(clean_line, args);
 
-		if (line[0] == '\0')
-			continue;
+        if (args[0] == NULL)
+            continue;
 
-		args[0] = line;
+        pid = fork();
+        if (pid == 0)
+        {
+            execvp(args[0], args);
+            perror(args[0]);
+            exit(EXIT_FAILURE);
+        }
+        else if (pid < 0)
+        {
+            perror("fork");
+        }
+        else
+        {
+            waitpid(pid, &status, 0);
+        }
+    }
 
-		pid = fork();
-		if (pid == -1)
-		{
-			perror("fork");
-			continue;
-		}
-
-		if (pid == 0)
-		{
-			if (execve(args[0], args, environ) == -1)
-			{
-				fprintf(stderr, "%s: No such file or directory\n", args[0]);
-				exit(EXIT_FAILURE);
-			}
-		}
-		else
-		{
-			wait(NULL);
-		}
-	}
-
-	free(line);
-	return (0);
+    return 0;
 }
