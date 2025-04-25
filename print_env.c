639ae9eac39da21e8cf198b2e09a46b9010df981
samueleangel <samueleangel@gmail.com>
2025-04-25 17:08:51 +0100

    Fix

--- print_env.c
+++ print_env.c
@@ -5,12 +5,14 @@
  */
 void print_env(void)
 {
-	char **env = environ;
+	int i = 0;
+	size_t len;
 
-	while (*env)
+	while (environ[i] != NULL)
 	{
-		write(STDOUT_FILENO, *env, strlen(*env);
+		len = strlen(environ[i]);
+		write(STDOUT_FILENO, environ[i], len);
 		write(STDOUT_FILENO, "\n", 1);
-		env++;
+		i++;
 	}
 }
