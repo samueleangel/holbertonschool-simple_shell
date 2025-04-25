cc60df4d8bdd74549cee0cb9a784ac400bb8fbd6
root <luismichelpcontacto@gmail.com>
2025-04-25 13:53:52 +0000

    shell changes

--- read_command.c
+++ read_command.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-#define  READ_SIZE 1024
+#define READ_SIZE 1024
 
 /**
  * read_command - Reads user input.
@@ -13,13 +13,12 @@ char *read_command(char **command, size_t *len)
 	static char buffer[READ_SIZE];
 	ssize_t bytes_read;
 	char *new_command = NULL;
-	size_t total_len = 0;
 
 	if (*command == NULL)
 		*len = 0;
 
-	while ((bytes_read = read(STDIN_FILENO, buffer, READ_SIZE-1)) > 0)
-	{	
+	while ((bytes_read = read(STDIN_FILENO, buffer, READ_SIZE - 1)) > 0)
+	{
 		buffer[bytes_read] = '\0';
 		new_command = realloc(*command, *len + bytes_read + 1);
 		if (!new_command)
@@ -31,18 +30,18 @@ char *read_command(char **command, size_t *len)
 		*command = new_command;
 		strcpy(*command + *len, buffer);
 		*len += bytes_read;
-		
+
 		if (strchr(buffer, '\n'))
 			break;
 	}
 	if (bytes_read <= 0 && *len == 0)
-		return (NULL);
+		return NULL;
 
-	if (*len > 0 && (*command)[*len-1] == '\n')
+	if (*len > 0 && (*command)[*len - 1] == '\n')
 	{
-		(*command){*len-1} = '\0';
+		(*command)[*len - 1] = '\0';
 		(*len)--;
 	}
-	
+
 	return *command;
 }
